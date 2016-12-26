5c6fdc4c2a6cf6ba6b4abc8119193d3e3d704fcc
Evgeny Zaritovskiy <ez@ez-macbook.local>
2016-12-25 23:12:06 -0500

    Red button works

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -13,10 +13,22 @@ class RatingControl: UIStackView {
     //MARK: Initialization 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupButtons()
     }
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK: Private Methods
+    private func setupButtons() {
+        let button = UIButton()
+        button.backgroundColor = .red
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        addArrangedSubview(button)
     }
     
 }
