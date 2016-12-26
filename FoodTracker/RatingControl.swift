6190f4cae39ba8b65a97131575fc9d944565507c
Evgeny Zaritovskiy <ez@ez-macbook.local>
2016-12-25 23:44:33 -0500

    Button pressed works

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -21,6 +21,11 @@ class RatingControl: UIStackView {
         setupButtons()
     }
     
+    //MARK: Button Actions
+    func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
+    }
+    
     //MARK: Private Methods
     private func setupButtons() {
         let button = UIButton()
@@ -28,6 +33,7 @@ class RatingControl: UIStackView {
         button.translatesAutoresizingMaskIntoConstraints = false
         button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
         button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
         addArrangedSubview(button)
     }
     
