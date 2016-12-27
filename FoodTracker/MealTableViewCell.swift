c055ae763936e273f3ddb9f26e057a735c0b0372
Evgeny Zaritovskiy <ez@ez-macbook.local>
2016-12-27 00:40:15 -0500

    Data source with sample data is shown in the table view

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Evgeny Zaritovskiy on 2016-12-26.
+//  Copyright © 2016 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
